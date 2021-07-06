e0227d5b06a865bb0a76ee048bc2fe2674d5b8ad
min8030 <a7965344@gmail.com>
2021-07-06 17:39:23 +0900

    POST_TEST

--- src/main/java/jnu/swlounge/controller/QuestionController.java
+++ src/main/java/jnu/swlounge/controller/QuestionController.java
@@ -1,15 +1,22 @@
 package jnu.swlounge.controller;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 
 @Controller
-@RequestMapping("/")
 public class QuestionController {
 
-
-
+    @PostMapping(path = "/enroll")
+    public ResponseEntity search(Question question) {
+        System.out.println(question.getQuestioner());
+        System.out.println(question.getWriter());
+        System.out.println(question.getLanguage());
+        System.out.println(question.getContent());
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
